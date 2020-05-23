1d9c0415cda682f46697b5719d14341f1e112fe5
jaime <jmfernandez1@miuandes.cl>
2020-05-23 11:09:53 -0400

    subir playlist

---
+++ Proyectog15WF/Model/Playlist.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Playlist
+    {
+        List<string> songs; // Quizas hacer una lista de objetos Song
+        List<string> random; // Quizas hacer una lista de objetos Song
+        List<bool> preferences;
+        string name;
+
+
+        // Dictionary<string, List<string>> followedplaylist = new Dictionary<string, List<string>>(); aplicar en user
+        //private Dictionary<int, string> queuelist; aplicar en user
+
+        // string lastPlayed;aplicar en user
+        // string nowPlaying;aplicar en user
+
+        public Playlist(string playlistname)
+        {
+            this.songs = new List<string>();
+            this.random = new List<string>();
+            this.preferences = new List<bool>() { false, false }; //privacy, download
+            this.name = playlistname;
+        }
+
+        // Get
+        public bool GetPlaylistPrivacy()
+        {
+            return preferences[0];
+        }
+        public bool GetPlaylistDownload()
+        {
+            return preferences[1];
+        }
+        public string GetPlaylistName()
+        {
+            return name;
+        }
+        public List<string> GetPlaylistAllSongs()
+        {
+            return songs;
+        }
+        //
+
+        public bool ChangePrivacy(bool input)
+        {
+            if (preferences[0] == true)
+            {
+                preferences[0] = false; // queda privada
+                return false;
+            }
+            if (preferences[0] == false)
+            {
+                preferences[0] = true; // queda publica
+                return true;
+            }
+            return false; // no paso nada por defecto setea la playlist a privada
+        }
+
+        public bool DownloadPlaylist(string premium) // ver si se cambia la variable premium
+        {
+            if (premium == "Premium")
+            {
+                if (preferences[0] == true)
+                {
+                    preferences[0] = false; // queda privada
+                    return false;
+                }
+                if (preferences[0] == false)
+                {
+                    preferences[0] = true; // queda publica
+                    return true;
+                }
+                return false;
+            }
+            else
+            {
+                return false; // no pasa nada por defecto no descarga la playlist
+            }
+        }
+
+        public bool AddSong(string songname)
+        {
+            foreach (string value in songs)
+            {
+                if (value == songname)
+                {
+                    return false; // no agrega la cancion porque ya está
+                }
+            }
+
+            songs.Add(songname);
+            return true; // si agrego la cancion
+        }
+
+        public bool RemoveSong(string songname)
+        {
+            foreach (string value in songs)
+            {
+                if (value == songname)
+                {
+                    return true; // La cancion es eliminada
+                }
+            }
+            return false; // La cancion no fue encontrada
+        }
+
+        public int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max); // Devuelve un numero random
+        }
+
+        public List<string> RandomPlaylistOrder()
+        {
+            random.Clear();
+            int counter = songs.Count();
+
+            foreach (string value in this.songs)
+            {
+                random.Add(songs[RandomNumber(1, counter + 1)]);
+                counter--;
+            }
+            return random; // devualve una mezcla de canciones de esta playlist
+        }
+    }
+}
---
+++ Proyectog15WF/Model/Queue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Queue
+    {
+        List<string> queue; // Quizas hacer la lista de objetos Song
+
+        public Queue()
+        {
+            this.queue = new List<string>();
+        }
+
+
+        public bool CheckSongsInQueue()
+        {
+            if (0 < queue.Count())
+            {
+                return true; // Hay canciones en Queue
+            }
+            return false;
+        }
+
+        public bool AddToQueue(string songName)
+        {
+            queue.Add(songName);
+            return true; // Se agrega cancion a la queue
+        }
+
+        public bool RemoveFromQueue(string songName)
+        {
+            if (0 < queue.Count())
+            {
+                foreach (string value in queue)
+                {
+                    if (value == songName)
+                    {
+                        queue.Remove(songName);
+                        return true; // Cancion agregada
+                    }
+                }
+            }
+            return false; // No se elimino la cancion o no hay canciones en queue
+        }
+    }
+}
